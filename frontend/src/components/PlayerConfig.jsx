91f7fee869b30c761ee1e1d968d46732a2ba78e0
d3meter <mark.demeter3@gmail.com>
2023-05-22 17:20:21 +0200

    implement player configs

--- frontend/src/components/PlayerConfig.jsx
+++ frontend/src/components/PlayerConfig.jsx
@@ -1,15 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-function PlayerConfig({ user, onLogoutPlayer }) {
+function PlayerConfig({
+  user,
+  onLogoutPlayer,
+  onNameChange,
+  onColorChange,
+  onFigureChange,
+}) {
 
- const handleLogout = () => {
-  onLogoutPlayer();
-};
+  const handleLogout = () => {
+    onLogoutPlayer();
+  };
+
+  const handleNameChange = (event) => {
+    onNameChange(event.target.value);
+  };
+
+  const handleColorChange = (event) => {
+    onColorChange(event.target.value);
+  };
+
+  const handleFigureChange = (event) => {
+    onFigureChange(event.target.value);
+  };
 
   return (
     <div>
       <p>{user.email}</p>
       <button onClick={handleLogout}>Logout</button>
+      <input onChange={handleNameChange} type="text" placeholder="name" />
+      <select onChange={handleFigureChange} name="figure" id="figure">
+        <option value="triangle">Triangle</option>
+        <option value="x">X</option>
+        <option value="circle">Cicle</option>
+      </select>
+      <select onChange={handleColorChange} name="color" id="color">
+        <option value="red">red</option>
+        <option value="green">green</option>
+        <option value="yellow">yellow</option>
+      </select>
     </div>
   );
 }
--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -7,10 +7,43 @@ import PlayerConfig from "../components/PlayerConfig";
 
 function Main() {
   const [isDisabled, setIsDisabled] = useState(true);
-  const [user1, setUser1] = useState(null);
-  const [user2, setUser2] = useState(null);
   const [isLoggedOut, setIsLoggedOut] = useState(false);
   const [sectionHeight, setsectionHeight] = useState("auto");
+
+  const [user1, setUser1] = useState(null);
+  const [user2, setUser2] = useState(null);
+
+  const [nameOfUser1, setNameOfUser1] = useState("player1");
+  const [colorOfUser1, setColorOfUser1] = useState("red");
+  const [figureOfUser1, setFigureOfUser1] = useState("circle");
+
+  const [nameOfUser2, setNameOfUser2] = useState("player2");
+  const [colorOfUser2, setColorOfUser2] = useState("blue");
+  const [figureOfUser2, setFigureOfUser2] = useState("x");
+
+  const onNameChangeP1 = (value) => {
+    setNameOfUser1(value);
+  };
+
+  const onNameChangeP2 = (value) => {
+    setNameOfUser2(value);
+  };
+
+  const onColorChangeP1 = (value) => {
+    setColorOfUser1(value);
+  };
+
+  const onColorChangeP2 = (value) => {
+    setColorOfUser2(value);
+  };
+
+  const onFigureChangeP1 = (value) => {
+    setFigureOfUser1(value);
+  };
+
+  const onFigureChangeP2 = (value) => {
+    setFigureOfUser2(value);
+  };
 
   useEffect(() => {
     setIsLoggedOut(!(user1 && user2));
@@ -65,15 +98,39 @@ function Main() {
           <div className="col-md-5 col-lg-5">
             <h1>Player 1</h1>
             {!!user1 ? (
-              <PlayerConfig user={user1} onLogoutPlayer={handleLogoutPlayer1} />
+              <>
+                <p>{nameOfUser1}</p>
+                <p>{colorOfUser1}</p>
+                <p>{figureOfUser1}</p>
+                <PlayerConfig
+                  user={user1}
+                  onLogoutPlayer={handleLogoutPlayer1}
+                  onNameChange={onNameChangeP1}
+                  onColorChange={onColorChangeP1}
+                  onFigureChange={onFigureChangeP1}
+                />
+              </>
             ) : (
-              <Login onLoginPlayer={handleLoginPlayer1} />
+              <Login
+                onLoginPlayer={handleLoginPlayer1}
+              />
             )}
           </div>
           <div className="col-md-5 col-lg-5">
             <h1>Player 2</h1>
             {!!user2 ? (
-              <PlayerConfig user={user2} onLogoutPlayer={handleLogoutPlayer2} />
+              <>
+                <p>{nameOfUser2}</p>
+                <p>{colorOfUser2}</p>
+                <p>{figureOfUser2}</p>
+                <PlayerConfig
+                  user={user2}
+                  onLogoutPlayer={handleLogoutPlayer2}
+                  onNameChange={onNameChangeP2}
+                  onColorChange={onColorChangeP2}
+                  onFigureChange={onFigureChangeP2}
+                />
+              </>
             ) : (
               <Login onLoginPlayer={handleLoginPlayer2} />
             )}
@@ -82,8 +139,10 @@ function Main() {
         <hr />
         {isLoggedOut && (
           <div
-          className={`row text-center collapse-section ${!isLoggedOut ? "hide" : ""}`}
-          style={{ height: sectionHeight }}
+            className={`row text-center collapse-section ${
+              !isLoggedOut ? "hide" : ""
+            }`}
+            style={{ height: sectionHeight }}
           >
             <Link to="/registration">
               <span>Or you can </span>
