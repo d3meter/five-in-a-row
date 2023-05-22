a0396dc7f99d40a5d66475e56d14e497942611bd
d3meter <mark.demeter3@gmail.com>
2023-05-22 16:07:48 +0200

    change Main

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -9,6 +9,22 @@ function Main() {
   const [isDisabled, setIsDisabled] = useState(true);
   const [user1, setUser1] = useState(null);
   const [user2, setUser2] = useState(null);
+  const [isLoggedOut, setIsLoggedOut] = useState(false);
+  const [sectionHeight, setsectionHeight] = useState("auto");
+
+  useEffect(() => {
+    setIsLoggedOut(!(user1 && user2));
+  }, [user1, user2]);
+
+  useEffect(() => {
+    if (!isLoggedOut) {
+      setTimeout(() => {
+        setsectionHeight("0");
+      }, 1000);
+    } else {
+      setsectionHeight("auto");
+    }
+  }, [isLoggedOut]);
 
   const handleLoginPlayer1 = (email, password) => {
     login(email, password).then((userData1) => setUser1(userData1));
@@ -33,21 +49,23 @@ function Main() {
   return (
     <div className="Main">
       <div className="container">
-        <div className="row">
-          <div className="col text-center">
-            <h2>Five in a Row (for 2 players)</h2>
-            <span>In order to play, both players need to be logged in.</span>
+        {isLoggedOut && (
+          <div
+            className={`row ${!isLoggedOut ? "hide" : ""}`}
+            style={{ height: sectionHeight }}
+          >
+            <div className="col text-center">
+              <h2>Five in a Row (for 2 players)</h2>
+              <span>In order to play, both players need to be logged in.</span>
+            </div>
           </div>
-        </div>
+        )}
         <hr />
         <div className="row gap-5 justify-content-center">
           <div className="col-md-5 col-lg-5">
             <h1>Player 1</h1>
             {!!user1 ? (
-              <PlayerConfig
-                user={user1}
-                onLogoutPlayer={handleLogoutPlayer1}
-              />
+              <PlayerConfig user={user1} onLogoutPlayer={handleLogoutPlayer1} />
             ) : (
               <Login onLoginPlayer={handleLoginPlayer1} />
             )}
@@ -55,22 +73,24 @@ function Main() {
           <div className="col-md-5 col-lg-5">
             <h1>Player 2</h1>
             {!!user2 ? (
-              <PlayerConfig
-                user={user2}
-                onLogoutPlayer={handleLogoutPlayer2}
-              />
+              <PlayerConfig user={user2} onLogoutPlayer={handleLogoutPlayer2} />
             ) : (
               <Login onLoginPlayer={handleLoginPlayer2} />
             )}
           </div>
         </div>
         <hr />
-        <div className="row text-center">
-          <Link to="/registration">
-            <span>Or you can </span>
-            <h2>REGISTER HERE</h2>
-          </Link>
-        </div>
+        {isLoggedOut && (
+          <div
+          className={`row text-center collapse-section ${!isLoggedOut ? "hide" : ""}`}
+          style={{ height: sectionHeight }}
+          >
+            <Link to="/registration">
+              <span>Or you can </span>
+              <h2>REGISTER HERE</h2>
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );
