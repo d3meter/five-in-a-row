1cf245f8aede67d4a2489327cc8f2b656355e9f1
d3meter <mark.demeter3@gmail.com>
2023-05-22 14:25:30 +0200

    start change structure with Main

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Layout from "./components/Layout";
 import Main from "./pages/Main";
 import Registration from "./pages/Registration";
 import Game from "./pages/Game";
@@ -10,11 +9,9 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Layout />}>
           <Route path="/" element={<Main />} />
           <Route path="/registration" element={<Registration />} />
           <Route path="/game" element={<Game />} />
-        </Route>
       </Routes>
     </Router>
   );
--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -2,6 +2,8 @@ import React, { useContext, useEffect, useState } from "react";
 import "./css/Main.css";
 import { Link } from "react-router-dom";
 import UserContext from "../admin/UserContext";
+import LoginPlayer1 from "../components/LoginPlayer1";
+import LoginPlayer2 from "../components/LoginPlayer2";
 
 function Main() {
   const { user1, user2 } = useContext(UserContext);
@@ -14,12 +16,29 @@ function Main() {
   return (
     <div className="Main">
       <div className="container">
-        <h1>Welcome</h1>
-        <Link to="/registration">Registration</Link>
+        <div className="row">
+          <div className="col text-center">
+            <h1>Five in a Row (for 2 players)</h1>
+            <span>In order to play, both players need to be logged in.</span>
+          </div>
+        </div>
+        <hr />
+        <div className="row gap-5 justify-content-center">
+          <div className="col-md-5 col-lg-5">
+            <LoginPlayer1 />
+          </div>
+          <div className="col-md-5 col-lg-5">
+            <LoginPlayer2 />
+          </div>
+        </div>
+        <hr />
+        <div className="row text-center">
+          <Link to="/registration">
+            <span>Or you can </span>
+            <h2>REGISTER HERE</h2>
+          </Link>
+        </div>
       </div>
-      <Link to="/game">
-        <button disabled={isDisabled}>Play</button>
-      </Link>
     </div>
   );
 }
